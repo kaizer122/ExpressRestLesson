468784c96ba87b104525e1e966fa0f4729977165
Kais Ladjemi <kladjemi@toolynk.com>
2020-12-09 22:21:13 +0100

    Cascade delete

--- src/models/BootcampCategory.js
+++ src/models/BootcampCategory.js
@@ -1,5 +1,6 @@
 import moment from "moment";
 import { model, Schema } from "mongoose";
+import Bootcamp from "./Bootcamp";
 const BootcampCategory = new Schema({
   name: {
     type: String,
@@ -10,6 +11,15 @@ const BootcampCategory = new Schema({
     type: Number,
     default: moment().valueOf(),
   },
+});
+
+// Cascade delete courses when a bootcamp is deleted
+BootcampCategory.pre("findOneAndDelete", async function (next) {
+  const res = await Bootcamp.deleteMany({ category: this._conditions._id });
+  console.log(
+    `${res.deletedCount} Bootcamps being removed because category ${this._conditions._id} was deleted`
+  );
+  next();
 });
 
 export default model("BootcampCategory", BootcampCategory);
