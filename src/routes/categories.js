acd8524b12c97d0921ce575f0c59b3813f8b5e2d
kais <kais.ladjemi@esprit.tn>
2020-12-11 09:34:59 +0100

    bug fix

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -5,10 +5,10 @@ const CategoriesRouter = express.Router();
 
 CategoriesRouter.route("/")
   .get(Controller.getCategories)
-  .post(protect, authorize(["admin"]), Controller.addCategory);
+  .post(protect, authorize("admin"), Controller.addCategory);
 
 CategoriesRouter.route("/:id")
   .put(Controller.updateCategory)
-  .delete(protect, authorize(["admin"]), Controller.deleteCategory);
+  .delete(protect, authorize("admin"), Controller.deleteCategory);
 
 export default CategoriesRouter;
